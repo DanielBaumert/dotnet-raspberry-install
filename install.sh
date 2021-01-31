2ef867e081daf6bbb58aa55790162687d64ce9fc
Daniel Baumert <daniel.baumert22@gmail.com>
2021-01-31 12:03:18 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 mkdir /opt/dotnet
 
 # donwload and install
-wget https://download.visualstudio.microsoft.com/download/pr/726e260e-ce94-46c3-a169-57b2ebf5433d/5fb2a00b04b3509a0a6db$
+wget https://download.visualstudio.microsoft.com/download/pr/726e260e-ce94-46c3-a169-57b2ebf5433d/5fb2a00b04b3509a0a6db63e302523a8/dotnet-sdk-5.0.102-linux-arm.tar.gz
 tar zxf /opt/dotnet/dotnet-sdk-5.0.102-linux-arm.tar.gz -C /opt/dotnet
 rm /opt/dotnet/dotnet-sdk-5.0.102-linux-arm.tar.gz
 
